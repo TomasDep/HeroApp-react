f2f855686ddb3199d0a9d982cf821b3db98ec110
TomasDep <tomas.valenzuelacb@gmail.com>
2022-12-04 17:58:50 -0300

    se cambia directorio

--- src/main.tsx
+++ src/main.tsx
@@ -1,8 +1,9 @@
 import React from "react";
+import { BrowserRouter } from "react-router-dom";
 import ReactDOM from "react-dom/client";
+
 import "./styles.css";
 import { HeroesApp } from "./HeroesApp";
-import { BrowserRouter } from "react-router-dom";
 
 ReactDOM.createRoot(document.getElementById("root") as HTMLElement).render(
   <React.StrictMode>
